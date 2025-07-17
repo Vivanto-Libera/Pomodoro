9d08be80538028d4461fdd55a35c460e8f63e0ba
Vivanto <1425078256@qq.com>
2025-07-17 18:55:47 +0800

    Now note can set bold, italic, and underline.

--- notewindow.cpp
+++ notewindow.cpp
@@ -12,3 +12,30 @@ NoteWindow::~NoteWindow()
 {
     delete ui;
 }
+
+void NoteWindow::on_act_bold_triggered(bool checked)
+{
+    QTextCharFormat fmt= ui->plainTextEdit->currentCharFormat();
+    if(checked)
+        fmt.setFontWeight(QFont::Bold);
+    else
+        fmt.setFontWeight(QFont::Normal);
+    ui->plainTextEdit->mergeCurrentCharFormat(fmt);
+}
+
+
+void NoteWindow::on_act_italic_triggered(bool checked)
+{
+    QTextCharFormat fmt= ui->plainTextEdit->currentCharFormat();
+    fmt.setFontItalic(checked);
+    ui->plainTextEdit->mergeCurrentCharFormat(fmt);
+}
+
+
+void NoteWindow::on_act_underline_triggered(bool checked)
+{
+    QTextCharFormat fmt= ui->plainTextEdit->currentCharFormat();
+    fmt.setFontUnderline(checked);
+    ui->plainTextEdit->mergeCurrentCharFormat(fmt);
+}
+
