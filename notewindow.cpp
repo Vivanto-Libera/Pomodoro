a7d39f54e3c0d293a738dcb2da3e0972aaed0f81
Vivanto <1425078256@qq.com>
2025-07-17 19:13:13 +0800

    Now can read notes.

--- notewindow.cpp
+++ notewindow.cpp
@@ -1,11 +1,29 @@
 #include "notewindow.h"
 #include "ui_notewindow.h"
-
+QDataStream &operator<<(QDataStream &out,const NoteWindow::note &aNote)
+{
+    out<<aNote.title<<aNote.text;
+    return out;
+}
+QDataStream &operator>>(QDataStream &in,NoteWindow::note &aNote)
+{
+    in>>aNote.title>>aNote.text;
+    return in;
+}
 NoteWindow::NoteWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::NoteWindow)
 {
     ui->setupUi(this);
+    if(!QFile::exists("notes.bin"))
+    {
+        ui->noteCombo->addItem(tr("未命名笔记"));
+        note aNote={ui->noteCombo->itemText(0),""};
+        notes<<aNote;
+    }
+    else
+        readNotes();
+    ui->plainTextEdit->appendPlainText(notes.at(0).text);
 }
 
 NoteWindow::~NoteWindow()
@@ -37,5 +55,20 @@ void NoteWindow::on_act_underline_triggered(bool checked)
     QTextCharFormat fmt= ui->plainTextEdit->currentCharFormat();
     fmt.setFontUnderline(checked);
     ui->plainTextEdit->mergeCurrentCharFormat(fmt);
+}
+
+void NoteWindow::readNotes()
+{
+    QFile aFile("notes.bin");
+    if(!aFile.open(QIODevice::ReadOnly))
+        return;
+    QDataStream fileStream(&aFile);
+    fileStream.setVersion(QDataStream::Qt_6_9);
+    fileStream.setByteOrder(QDataStream::BigEndian);
+    while(!fileStream.atEnd())
+    {
+        fileStream>>notes;
+    }
+    aFile.close();
 }
 
--- notewindow.h
+++ notewindow.h
@@ -2,6 +2,7 @@
 #define NOTEWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
 
 namespace Ui {
 class NoteWindow;
@@ -14,9 +15,23 @@ class NoteWindow : public QMainWindow
 public:
     explicit NoteWindow(QWidget *parent = nullptr);
     ~NoteWindow();
+    struct note
+    {
+        QString title;
+        QString text;
+    };
+
+private slots:
+    void on_act_bold_triggered(bool checked);
+
+    void on_act_italic_triggered(bool checked);
+
+    void on_act_underline_triggered(bool checked);
 
 private:
     Ui::NoteWindow *ui;
+    QList<note> notes;
+    void readNotes();
 };
 
 #endif // NOTEWINDOW_H
